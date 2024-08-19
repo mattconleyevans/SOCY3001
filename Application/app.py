cb1107e4444a1beb0f324c81330b6bab2e66b1c6
mattconleyevans <mattconleyevans@gmail.com>
2024-08-19 13:40:34 +1000

    Add data to Flask project

---
+++ Application/app.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, jsonify, send_from_directory
+from app.query import queryOpenAI
+import os
+
+app = Flask(__name__, static_folder='frontend/build')
+
+@app.route('/query', methods=['POST'])
+def query():
+    data = request.json
+    query_text = data.get('query_text')
+    response = queryOpenAI(query_text, image_index, text_index, text_documents, image_documents)
+    return jsonify({"response": response.choices[0].message.content})
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def serve(path):
+    if path != "" and os.path.exists(app.static_folder + '/' + path):
+        return send_from_directory(app.static_folder, path)
+    else:
+        return send_from_directory(app.static_folder, 'index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
