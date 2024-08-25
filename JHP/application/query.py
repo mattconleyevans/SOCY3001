8b0e941362fe275421e9e9e692fe1342d4f8fe69
mattconleyevans <mattconleyevans@gmail.com>
2024-08-26 01:20:46 +1000

    Fix image search

--- JHP/application/query.py
+++ JHP/application/query.py
@@ -78,7 +78,7 @@ def queryOpenAI(query_text, image_index, text_index, images, texts):
             "message": openai_response,
             "images": [
                 {"url": images.iloc[i, 1], "caption": images.iloc[i, 2]}
-                for i in range(len(image_indices))
+                for i in image_indices
             ]
         }
         return response
